2a107e7950670995a7c75737d200ea8fdcbdda5e
Michał Zakrzewski <miczak96@gmail.com>
2018-10-09 20:26:48 +0200

    Slight locator.py remake

--- mrc/localization/locator.py
+++ mrc/localization/locator.py
@@ -1,23 +1,35 @@
 import threading
-from dynaconf import settings
-import itertools
 import time
+from typing import Callable
 
 
 class Locator(threading.Thread):
-    def __init__(self):
+    def __init__(self, read_data: Callable, process_data: Callable, interval: float):
         threading.Thread.__init__(self)
-        self.locations = dict(itertools.product(settings.get('ROBOTS'), [None]))
-        self.data_reader = settings.get('DATA_READER')
-        self.data_processor = settings.get('DATA_PROCESSOR')
-        self.interval = settings.get('INTERVAL', 0)
+        self.locations = dict()
+        self.read_data = read_data
+        self.process_data = process_data
+        self.interval = interval
         self.running = False
 
     def run(self):
         self.running = True
         while self.running:
-            self.__update_locations()
+            self._update_locations()
             time.sleep(self.interval)
+
+    def update_and_get_locations(self, robot_id=None):
+        if not self.is_running():
+            self._update_locations()
+        return self.get_locations(robot_id)
+
+    def _update_locations(self):
+        raw_data = self.read_data()
+        processed_data = self.process_data(raw_data) if self.process_data is not None else raw_data
+        self.locations = processed_data
+
+    def get_locations(self, robot_id=None):
+        return self.locations[robot_id] if robot_id is not None else self.locations
 
     def stop(self):
         self.running = False
@@ -25,12 +37,5 @@ class Locator(threading.Thread):
     def is_running(self):
         return self.running
 
-    def __update_locations(self):
-        raw_data = self.data_reader()
-        processed_data = self.data_processor(raw_data) if self.data_processor is not None else raw_data
-        self.locations = processed_data
-
-    def get_locations(self, robot_id=None):
-        if not self.is_running():
-            self.__update_locations()
-        return self.locations[robot_id] if robot_id is not None else self.locations
+    def set_interval(self, interval: float):
+        self.interval = interval
