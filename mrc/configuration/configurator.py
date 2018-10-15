772cdd9faed17a8f30b1857925b2e1cf2aa73926
Michał Zakrzewski <miczak96@gmail.com>
2018-10-15 12:55:21 +0200

    Code review follow-up #1

--- mrc/configuration/configurator.py
+++ mrc/configuration/configurator.py
@@ -2,20 +2,20 @@ from mrc.shared.position import Position
 
 
 class Configurator:
-    def __init__(self, id_to_location):
-        if callable(id_to_location):
+    def __init__(self, id_to_location_mapper):
+        if callable(id_to_location_mapper):
             self.is_callable = True
-        elif isinstance(id_to_location, dict):
+        elif isinstance(id_to_location_mapper, dict):
             self.is_callable = False
         else:
             raise ValueError("id_to_location should be either callable or dictionary")
-        self.id_to_location = id_to_location
+        self.id_to_location_mapper = id_to_location_mapper
 
     def _extract_location(self, key) -> Position:
         if self.is_callable:
-            return self.id_to_location(key)
+            return self.id_to_location_mapper(key)
         else:
-            return self.id_to_location.get(key)
+            return self.id_to_location_mapper.get(key)
 
     def get_target_position(self, robot_id) -> Position:
         return self._extract_location(robot_id)
--- mrc/control/controller.py
+++ mrc/control/controller.py
@@ -5,16 +5,19 @@ from mrc.localization.locator import Locator
 
 
 class Controller(Thread):
-    def __init__(self, locator: Locator, dtp_func: Callable([float, float], None)):
+    def __init__(self, locator: Locator, drive_to_point: Callable([float, float], None)):
         super().__init__()
-        self.dtp_func = dtp_func
+        self.drive_to_point = drive_to_point
         self.locator = locator
         self.running = False
 
     def run(self):
         self.running = True
-        while True:
+        while self.running:
             pass
 
     def is_running(self):
         return self.running
+
+    def stop(self):
+        self.running = False
