aed40bfc849ce68dbe338edfbdb77089545b6e5d
Michał Zakrzewski <miczak96@gmail.com>
2018-10-24 22:08:53 +0200

    Changed dict/callable to only one target position

--- mrc/configuration/configurator.py
+++ mrc/configuration/configurator.py
@@ -2,21 +2,5 @@ from mrc.shared.relativeposition import RelativePosition
 
 
 class Configurator:
-    def __init__(self, my_id, id_to_location_mapper):
-        self.my_id = my_id
-        if callable(id_to_location_mapper):
-            self.is_callable = True
-        elif isinstance(id_to_location_mapper, dict):
-            self.is_callable = False
-        else:
-            raise ValueError("id_to_location should be either callable or dictionary")
-        self.id_to_location_mapper = id_to_location_mapper
-
-    def _extract_location(self, key) -> RelativePosition:
-        if self.is_callable:
-            return self.id_to_location_mapper(key)
-        else:
-            return self.id_to_location_mapper.get(key)
-
-    def get_target_position(self) -> RelativePosition:
-        return self._extract_location(self.my_id)
+    def __init__(self, target_position: RelativePosition):
+        self.target_position = target_position
