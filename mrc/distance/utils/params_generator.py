0a73d874a54f407446ca7ebfe06614e8f0f2d3f3
Lukasz1928 <Lukasz19281@gmail.com>
2018-11-18 19:23:17 +0100

    ParamsGenerator should work

--- mrc/distance/utils/params_generator.py
+++ mrc/distance/utils/params_generator.py
@@ -1,4 +1,6 @@
 import math
+from operator import itemgetter
+
 import cv2
 import numpy as np
 from mrc.localization.camera.projections import projections
@@ -24,29 +26,38 @@ class ParamsGenerator:
         thetas = [math.atan(rd / self.h) for rd in distances_in_reality]
 
         diodes_in_photo = [self._find_diode(img) for img in self.photos]
-        diodes_in_photo_vectors = [
-            (diodes_in_photo[i][0] - self.photo_centres[i][0], self.photo_centres[i][1] - diodes_in_photo[i][1]) for i
-            in range(len(diodes_in_photo))]
+        diodes_in_photo_vectors = [self._calculate_diode_vector(diodes_in_photo[i], self.photo_centres[i]) for i in
+                                   range(len(diodes_in_photo))]
         diodes_in_photo_normalized_vectors = [self._normalize_vector(v, self.photos[i].shape) for i, v in
                                               enumerate(diodes_in_photo_vectors)]
         normalized_vector_lengths = [self._vector_length(v) for v in diodes_in_photo_normalized_vectors]
 
         calculated_focals = [
-            self._calculate_focal_length(p, normalized_vector_lengths, thetas) for p in
+            list(self._calculate_focal_length(p, normalized_vector_lengths, thetas)) for p in
             projections]  # thetas: angle Ground-Camera-Diode
-        selected_projection, focal, _ = np.min(calculated_focals, axis=2)
+        selected_projection, focal, t = min(calculated_focals, key=itemgetter(2))
         return selected_projection, focal
 
     @staticmethod
+    def _calculate_diode_vector(diode_in_photo, photo_centre):
+        if diode_in_photo is not None:
+            return diode_in_photo[0] - photo_centre[0], photo_centre[1] - diode_in_photo[1]
+        return None
+
+    @staticmethod
     def _normalize_vector(vector, resolution):
-        return vector[0] / resolution[0], vector[1] / resolution[1]
+        if vector is not None:
+            return vector[0] / resolution[0], vector[1] / resolution[1]
+        return None
 
     @staticmethod
     def _find_diode(image):
         gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         leds_image = cv2.inRange(gray_image, 225, 255)
         kpts = ParamsGenerator._prepare_blob_detector().detect(leds_image)
-        return kpts[0].pt
+        if len(kpts) > 0:
+            return kpts[0].pt
+        return None
 
     @staticmethod
     def _prepare_blob_detector():
@@ -59,17 +70,20 @@ class ParamsGenerator:
         params.filterByConvexity = False
         params.filterByArea = True
         params.maxArea = 250
-        params.minArea = 10
+        params.minArea = 1
         detector = cv2.SimpleBlobDetector_create(params)
         return detector
 
     @staticmethod
     def _vector_length(v):
-        return math.sqrt(v[0] ** 2 + v[1] ** 2)
+        if v is not None:
+            return math.sqrt(v[0] ** 2 + v[1] ** 2)
+        return None
 
     @staticmethod
     def _calculate_focal_length(projection, normalised_vector_lengths, theta):
         res = []
         for r, t in zip(normalised_vector_lengths, theta):
-            res.append(projection.focal(t, r))
+            if r is not None:
+                res.append(projection.focal(t, r))
         return projection, np.mean(res), np.std(res)
