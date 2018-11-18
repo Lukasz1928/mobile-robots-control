9cfa6ca1a87c2cae0edfc27aae3e0dc6999cd329
Lukasz1928 <Lukasz19281@gmail.com>
2018-11-18 18:29:56 +0100

    Changed CircularIterator

--- mrc/localization/utils/camera_locator.py
+++ mrc/localization/utils/camera_locator.py
@@ -1,8 +1,12 @@
+import colorsys
+from typing import Tuple
+
 import numpy
 import cv2
-# from picamera import PiCamera
-# from picamera.array import PiRGBArray
+#from picamera import PiCamera
+#from picamera.array import PiRGBArray
 import math
+import numpy as np
 
 
 class CameraReader:
@@ -10,6 +14,7 @@ class CameraReader:
         self.camera = PiCamera()
         self.camera_resolution = (math.ceil(resolution[1] / 16) * 16, math.ceil(resolution[0] / 32) * 32, 3)
         self.camera.resolution = resolution
+        self.camera.brightness = 30
 
     def __call__(self):
         frame = numpy.empty(self.camera_resolution, dtype=numpy.uint8)
@@ -18,8 +23,7 @@ class CameraReader:
 
 
 class CircularIterator:
-    def __init__(self, image, centre, radius):
-        self.image = image
+    def __init__(self, centre, radius):
         self.centre = centre
         self.radius = radius
         self.current_location_vector = [0, radius + 1]
@@ -52,7 +56,7 @@ class CircularIterator:
             except ValueError:
                 raise StopIteration
             self.current_location_vector[0] = -1 * self.x_range - 1
-        return self.image[self.__current_location()]
+        return self.__current_location()
 
 
 class FisheyeCameraLocator:
@@ -61,26 +65,33 @@ class FisheyeCameraLocator:
         self.blob_detector = FisheyeCameraLocator.prepare_blob_detector()
 
     def __call__(self, image):
-        gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        leds_image = cv2.inRange(gray_image, 225, 255)
+        grayscale_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        leds_image = cv2.inRange(grayscale_image, 125, 255)
         kpts = self.blob_detector.detect(leds_image)
+        colors = {}
         for kp in kpts:
-            FisheyeCameraLocator.calculate_blob_color(kp, image)
-        image_with_keypoints = cv2.drawKeypoints(image, kpts, numpy.array([]), (0, 255, 0),
-                                                 cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        resolution = (750, 600)
-        r = (math.ceil(resolution[1] / 16) * 16, math.ceil(resolution[0] / 32) * 32, 3)
-        keypoints_image = cv2.drawKeypoints(numpy.empty(r, dtype=numpy.uint8), kpts, numpy.array([]), (0, 255, 0),
-                                            cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        cv2.imshow('keypoints_image', keypoints_image)
-        cv2.waitKey(1)
-        return image_with_keypoints
+            keypoint_color = FisheyeCameraLocator.calculate_blob_color(kp, image)
+            colors[kp.pt] = keypoint_color
+        return colors
 
     @staticmethod
     def calculate_blob_color(keypoint, image):
         centre = (int(keypoint.pt[1]), int(keypoint.pt[0]))
         radius = int(keypoint.size / 2)
-        return numpy.mean([pixel for pixel in CircularIterator(img, centre, radius)], axis=0)
+        return numpy.mean([image[pt[0]][pt[1]] for pt in CircularIterator(centre, radius)], axis=0)
+
+    @staticmethod
+    def bgr2hsv(bgr):
+        arr = np.ndarray([1, 1, 3], dtype=np.uint8)
+        arr[0][0] = bgr
+        hsv = cv2.cvtColor(arr, cv2.COLOR_BGR2HSV)
+        return [hsv[0][0][0] * 2, hsv[0][0][1] / 2.55, hsv[0][0][2] / 2.55]
+
+    # TODO
+    @staticmethod
+    def calculate_point_weight(point, centre, radius):
+        if (point[0] - centre[0]) ** 2 + (point[1] - centre[1]) ** 2 < radius ** 2 / 4:
+            return 1
 
     @staticmethod
     def prepare_blob_detector():
@@ -88,20 +99,28 @@ class FisheyeCameraLocator:
         params.filterByColor = True
         params.blobColor = 255
         params.filterByCircularity = True
-        params.minCircularity = 0.5
+        params.minCircularity = 0.4
         params.filterByInertia = False
         params.filterByConvexity = False
         params.filterByArea = True
         params.maxArea = 250
-        params.minArea = 10
+        params.minArea = 0
         detector = cv2.SimpleBlobDetector_create(params)
         return detector
 
+
+img = cv2.imread('C:/Users/Lukasz1928/Desktop/inz/a/img.jpg')
+loc = FisheyeCameraLocator(None)
+colors = loc(img)
+for key in colors.keys():
+    colors[key] = FisheyeCameraLocator.bgr2hsv(colors[key])
+print(colors)
 
 # reader = CameraReader((750, 600))
 # processor = FisheyeCameraLocator(None)
 # while True:
 #     img = reader()
+#     cv2.imshow('img', img)
 #     p = processor(img)
 #     if cv2.waitKey(1) & 0xFF == ord('q'):
 #         break
