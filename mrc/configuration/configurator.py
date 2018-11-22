9449059e4ef65beafb1a12d52db4d584570b221d
Michał Zakrzewski <miczak96@gmail.com>
2018-11-22 21:51:42 +0100

    Added self identity to Configurator

--- mrc/configuration/configurator.py
+++ mrc/configuration/configurator.py
@@ -1,7 +1,5 @@
-from mrc.shared.relativeposition import RelativePosition
-
-
 class Configurator:
-    def __init__(self, master_unit, target_position: RelativePosition):
+    def __init__(self, master_unit, target_position, identity):
         self.master_unit = master_unit
         self.target_position = target_position
+        self.identity = identity
