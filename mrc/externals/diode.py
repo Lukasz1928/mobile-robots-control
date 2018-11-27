dede6c277f95d38c61b901a31ac456e0e160cda2
zdzarsky <wojtek.zdz@gmail.com>
2018-11-27 18:16:45 +0100

    Quick review changes.

--- mrc/externals/diode.py
+++ mrc/externals/diode.py
@@ -4,11 +4,7 @@ import wiringpi
 class WS2811:
     def __init__(self, rgb, spi_ch=1, spi_speed=2000000):
         self._verify_color(rgb)
-        self.rgb = {
-            'r': rgb[0],
-            'g': rgb[1],
-            'b': rgb[2]
-        }
+        self.rgb = rgb
         self.spi_ch = spi_ch
         self.spi_speed = spi_speed
         wiringpi.wiringPiSetupGpio()
@@ -17,11 +13,13 @@ class WS2811:
     def __call__(self):
         self.send_color_via_spi()
 
-    def __setitem__(self, key, value):
-        if type(value) is int and value in range(0, 256) and key in ['r,g,b']:
-            self.rgb[key] = value
-        else:
-            raise ValueError("value must be from range 0-255 and keys must be from 'r','g','b'")
+    def send_color_via_spi(self):
+        wiringpi.wiringPiSPIDataRW(self.spi_ch, bytes(self.rgb))
+
+    def set_diode_color(self, rgb):
+        self._verify_color(rgb)
+        self.rgb = rgb
+        self.send_color_via_spi()
 
     @staticmethod
     def _verify_color(rgb):
@@ -32,14 +30,4 @@ class WS2811:
                 raise ValueError("Diode colors must be an iterable structure [r,g,b] for rgb in ranges 0,255")
         return True
 
-    def send_color_via_spi(self):
-        values = [self.rgb['r'], self.rgb['g'], self.rgb['b']]
-        wiringpi.wiringPiSPIDataRW(self.spi_ch, bytes(values))
-
-    def change_color(self, rgb):
-        self._verify_color(rgb)
-        self.rgb['r'] = rgb[0]
-        self.rgb['g'] = rgb[1]
-        self.rgb['b'] = rgb[2]
-        self.send_color_via_spi()
 
