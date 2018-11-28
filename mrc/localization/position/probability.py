ca5ff9d32b498762e91990f0f4297d1526cf9153
zdzarsky <wojtek.zdz@gmail.com>
2018-11-28 23:15:42 +0100

    Changed package name.

--- mrc/localization/position/probability.py
+++ mrc/localization/position/probability.py
@@ -9,14 +9,14 @@ class AngularProbability:
         self.stdev = stdev
         self.coef = coef
 
-    def __call__(self, shift):
-        return gauss_distribution(shift, self.mean, self.stdev)
+    def __call__(self, rotation):
+        return gauss_distribution(rotation, self.mean, self.stdev)
 
-    def rotate(self, shift):
-        mean = (self.mean + shift) % (2 * math.pi) - math.pi
+    def shift(self, rotation):
+        mean = (self.mean + rotation) % (2 * math.pi) - math.pi
         if self.mean > math.pi:
             self.mean = mean - 2 * math.pi
         else:
             self.mean = mean
         self.stdev = self.coef * self.stdev
-        return gauss_distribution(shift, self.mean, self.stdev)
+        return gauss_distribution(rotation, self.mean, self.stdev)
