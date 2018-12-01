7cddf663aa91d0ae787e09fa40b49d5840c163fd
Michał Zakrzewski <miczak96@gmail.com>
2018-12-01 21:01:23 +0100

    Added pydoc to configurator Minor fix to constructor

--- mrc/configuration/configurator.py
+++ mrc/configuration/configurator.py
@@ -1,14 +1,37 @@
 
 
 class Configurator:
+    """
+    Container for data about robot's position in formation.
+    """
     def __init__(self, master_unit, target_position, identity, set_external_identity=None):
+        """
+        Parameters
+        ----------
+        master_unit : Any
+            ID of master unit. It has to be compatible with locator.
+        target_position : (double, double)
+            qwerty
+        identity : Any
+            ID of robot. It has to be compatible with locator.
+        set_external_identity : :obj:`Any -> Any`, optional
+            Optional function responsible for changing robot identity outside `Configurator`.
+            It has to accept `identity` as its only argument.
+        """
         self.master_unit = master_unit
         self.target_position = target_position
         self._identity = identity
         self._set_external_identity = set_external_identity
-        self._set_external_identity(self._identity)
+        self.set_identity(self._identity)
 
     def set_identity(self, identity):
+        """Function responsible for changing robot identity
+
+        Parameters
+        ----------
+        identity : Any
+            New ID of robot. It has to be compatible with locator.
+        """
         self._identity = identity
         if self._set_external_identity is not None:
             self._set_external_identity(self._identity)
