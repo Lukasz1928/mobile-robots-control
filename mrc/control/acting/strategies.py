74a5cf293034068b304350694d9f94f3da45c5b9
zdzarsky <wojtek.zdz@gmail.com>
2018-12-01 20:36:11 +0100

    Added prototype of drive to point strategy.

--- mrc/control/acting/strategies.py
+++ mrc/control/acting/strategies.py
@@ -1,7 +1,7 @@
-from random import random
-
 from mrc.control.acting.abstract import AbstractStrategy
+from mrc.control.movement_prediction.target_position_calculator import TargetPositionCalculator
 from mrc.control.steering.exceptions import ObstacleOnTheWayException, SteeringException
+from mrc.shared.position import PolarPosition
 
 
 class FollowMasterStrategy(AbstractStrategy):
@@ -10,32 +10,37 @@ class FollowMasterStrategy(AbstractStrategy):
         self.steering_strategy = steering_strategy
         self.locator = locator
         self.configurator = configurator
-
+        self.position_calculator = TargetPositionCalculator(self.configurator)
         self.locations = None
-        self.target_position = None
-        self.position_reached = False
+        self.current_step = (0, 0)
+        self.step_reached = True
+        self.driving = False
 
     def read(self):
-        self.locations = self.locator.get_locations()
-        self.target_position = self.configurator.target_position
+        if self.step_reached:
+            self.locations = self.locator.get_locations(None)
+            master_position = self.locator.get_locations(self.configurator.master_unit)
+            self.current_step = self.position_calculator.calculate_actual_target_position(master_position,
+                                                                                          self.current_step)
 
     def think(self):
-
-        x = random.choice([True, False])
-        if x:  # TODO : getPositionFromMichal is not reached
-            self.position_reached = False
-            self.target_position = 5  # TODO: getFromMichal
+        if PolarPosition.are_positions_approximately_same(self.current_step, (0, 0)):
+            self.step_reached = True
         else:
-            self.position_reached = True
-            self.target_position = 3
+            self.step_reached = False
 
     def act(self):
-        if not self.position_reached:
+        if not self.step_reached and not self.driving:
             try:
-                self.steering_strategy.drive_to_point(self.target_position)
+                self.steering_strategy.drive_to_point(self.current_step)
+                self.driving = True
             except ObstacleOnTheWayException as ootwe:
                 print(ootwe.message)
+                self.driving = False
             except SteeringException:
-                print("Nie da sie")
-        else:
-            print("Pojechalem jak szalony rozjezdzajac wszystko po drodze")
+                print("Something went wrong")  # TODO: Add logger to whole project
+                self.driving = False
+        elif self.step_reached and self.driving:
+            self.driving = False
+        else:  # We did not reach the step and still driving
+            print("Pyr pyr.")
