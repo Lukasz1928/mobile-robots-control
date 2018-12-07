13eb4a5738549f97c9d159324e1599b7185a4b41
Michał Zakrzewski <miczak96@gmail.com>
2018-12-07 15:03:26 +0100

    Added function responsible for unignoring constructors pystrings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -182,6 +182,17 @@ epub_title = project
 epub_exclude_files = ['search.html']
 
 
+# -- Unignoring constructor --------------------------------------------------
+
+def skip(app, what, name, obj, skip, options):
+    if name == "__init__":
+        return False
+    return skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
+
 # -- Extension configuration -------------------------------------------------
 
 # -- Options for intersphinx extension ---------------------------------------
