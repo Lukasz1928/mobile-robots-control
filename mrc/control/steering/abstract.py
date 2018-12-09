c77af0938cdfdb7c699ed2ca545ab3f7e24d0f74
Michał Zakrzewski <miczak96@gmail.com>
2018-12-09 14:35:56 +0100

    Control module documented Some minor refactors

--- mrc/control/steering/abstract.py
+++ mrc/control/steering/abstract.py
@@ -2,11 +2,36 @@ from abc import ABC, abstractmethod
 
 
 class AbstractDTPSteeringInterface(ABC):
+    """
+    Robot steering interface template.
+
+    Notes
+    -----
+    This class is abstract and needs to be overridden to be instantiated.
+    """
 
     @abstractmethod
     def drive_to_point(self, point):
+        """
+        Move unit to specified point
+
+        Parameters
+        ----------
+        point : (float, float)
+            Target position.
+            First field is distance, second field is angle.
+        """
         pass
 
     @abstractmethod
     def update_data(self, **kwargs):
+        """
+        Update information about environment in the steering interface.
+        Implemented in case it should have information about environment.
+
+        Parameters
+        ----------
+        kwargs
+            Named arguments for usage in method implementation
+        """
         pass
