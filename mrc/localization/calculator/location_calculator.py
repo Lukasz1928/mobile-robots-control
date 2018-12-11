0d7b183388050ad54f26e4b4e885035b56655807
Lukasz1928 <Lukasz19281@gmail.com>
2018-12-11 18:52:08 +0100

    Added exception raise when parameters are incorrectly set

--- mrc/localization/calculator/location_calculator.py
+++ mrc/localization/calculator/location_calculator.py
@@ -2,6 +2,7 @@ import math
 
 from mrc.distance.distance_function import get_distance_function_from_file
 from mrc.localization.calculator.abstract_calculator import AbstractLocationCalculator
+from mrc.shared.exceptions.exceptions import ParameterException
 from mrc.utils.maths import vector_2d_length, normalize_point_in_circle
 
 
@@ -9,6 +10,8 @@ class LocationCalculator(AbstractLocationCalculator):
     def __init__(self, resolution, circle_radius, distance_function=None, angle_function=None, filename=None):
         self.resolution = resolution
         self.image_circle_radius = circle_radius
+        if (distance_function is not None and filename is not None) or (distance_function is None and filename is None):
+            raise ParameterException("Exactly one of [distance_function, filename] parameters must be set")
         if distance_function is not None:
             self.distance_function = distance_function
         else:
